ba41e6763d7bab99f4c9d07efc3e5c9de3d2b789
Andrew Peterson <andy@ndpsoftware.com>
2020-01-25 14:07:20 -0800

    namespace

--- explore.spec.rb
+++ explore.spec.rb
@@ -14,35 +14,35 @@ end
 
 ############################
 # DSL
-require 'forwardable'
 
+module Thoreau
 # Represents a set of inputs or different setups.
 # Setting "values" can be:
 # * an enumerable, yielding each value (dynamic is fine). This
 #   can be a hard-coded array, or a dynamic randomizing function.
 # * a single value
-class SetupSet
+  class SetupSet
 
-  def initialize(desc, values)
-    @desc   = desc.to_sym
-    @values = values
-  end
+    def initialize(desc, values)
+      @desc   = desc.to_sym
+      @values = values
+    end
 
-  def each_setup(&block)
-    if @values.respond_to?(:each)
-      @values.each(&block)
-    else
-      block.call(@values)
+    def each_setup(&block)
+      if @values.respond_to?(:each)
+        @values.each(&block)
+      else
+        block.call(@values)
+      end
+    end
+
+    def each_setup_block(&block)
+      self.each_setup do |setup|
+        setup_block = Proc.new { |context| (setup.respond_to? :call) ? context.instance_eval(&setup) : setup }
+        block.call(setup_block)
+      end
     end
   end
-
-  def each_setup_block(&block)
-    self.each_setup do |setup|
-      setup_block = Proc.new { |context| (setup.respond_to? :call) ? context.instance_eval(&setup) : setup }
-      block.call(setup_block)
-    end
-  end
-end
 
 # Represents a post-condition of a test.
 # A test will be created for each assertion in the "one assertion per test" tradition.
@@ -50,102 +50,103 @@ end
 # * passed the result of the action as its first argument
 # * passed the setup value as the second argument
 # * will be executed in the same test context as the setup and action code
-class Assertion
+  class Assertion
 
-  attr_accessor :desc
+    attr_accessor :desc
 
-  def initialize(desc, block)
-    @desc  = desc.to_sym
-    @block = block
+    def initialize(desc, block)
+      @desc  = desc.to_sym
+      @block = block
+    end
+
+    def exec_in_context(context, result, setup_value)
+      context.instance_exec(result, setup_value, &@block)
+    end
+
   end
-
-  def exec_in_context(context, result, setup_value)
-    context.instance_exec(result, setup_value, &@block)
-  end
-
-end
 
 # Represents a group of tests with a set of one or more setups,
 # an `action_block`, and one or more assertions, or post-conditions.
-class EquivalenceClass
-  attr_accessor :setup_key
-  attr_accessor :asserts_keys
-  attr_accessor :setup
-  attr_accessor :action_block
-  attr_accessor :assertions
+  class EquivalenceClass
+    attr_accessor :setup_key
+    attr_accessor :asserts_keys
+    attr_accessor :setup
+    attr_accessor :action_block
+    attr_accessor :assertions
 
-  def initialize(setup_key, asserts_keys)
-    @setup_key    = setup_key.to_sym
-    @asserts_keys = [asserts_keys].flatten.map(&:to_sym)
-  end
+    def initialize(setup_key, asserts_keys)
+      @setup_key    = setup_key.to_sym
+      @asserts_keys = [asserts_keys].flatten.map(&:to_sym)
+    end
 
-  def inspect
-    "EquivalenceClass #{@setup_key} => #{@asserts_keys}"
-  end
+    def inspect
+      "EquivalenceClass #{@setup_key} => #{@asserts_keys}"
+    end
 
-  def each_test(&block)
-    @setup.each_setup_block do |setup_block|
-      @assertions.each do |assertion|
-        block.call(setup_block, @action_block, assertion)
+    def each_test(&block)
+      @setup.each_setup_block do |setup_block|
+        @assertions.each do |assertion|
+          block.call(setup_block, @action_block, assertion)
+        end
       end
     end
+
   end
 
-end
+  class SuiteDSL
 
-class SuiteDSL
+    attr_reader :equivalence_classes, :action_block, :assertions, :setup_set_hash
 
-  attr_reader :equivalence_classes, :action_block, :assertions, :setup_set_hash
+    def initialize
+      @action              = nil
+      @setup_set_hash      = {}
+      @assertions          = {}
+      @equivalence_classes = []
+    end
 
-  def initialize
-    @action              = nil
-    @setup_set_hash      = {}
-    @assertions          = {}
-    @equivalence_classes = []
-  end
+    def cases(hash)
+      @equivalence_classes = hash.keys.map { |k| EquivalenceClass.new(k, hash[k]) }
+    end
 
-  def cases(hash)
-    @equivalence_classes = hash.keys.map { |k| EquivalenceClass.new(k, hash[k]) }
-  end
+    def setup(setup_key, values = nil, &block)
+      @setup_set_hash[setup_key.to_sym] = SetupSet.new(setup_key, values || block)
+    end
 
-  def setup(setup_key, values = nil, &block)
-    @setup_set_hash[setup_key.to_sym] = SetupSet.new(setup_key, values || block)
-  end
+    def action(&block)
+      @action_block = block
+    end
 
-  def action(&block)
-    @action_block = block
-  end
+    def asserts(desc, &block)
+      @assertions[desc.to_sym] = Assertion.new(desc, block)
+    end
 
-  def asserts(desc, &block)
-    @assertions[desc.to_sym] = Assertion.new(desc, block)
-  end
+    def verify_config!
+      @equivalence_classes.each do |ec|
+        s = @setup_set_hash[ec.setup_key]
+        puts "# WARNING: Setup not defined for `#{ec.setup_key}` so null setup will be used. Defined: #{@setup_set_hash.keys}" unless s
 
-  def verify_config!
-    @equivalence_classes.each do |ec|
-      s = @setup_set_hash[ec.setup_key]
-      puts "# WARNING: Setup not defined for `#{ec.setup_key}` so null setup will be used. Defined: #{@setup_set_hash.keys}" unless s
-
-      ec.asserts_keys.each do |key|
-        raise "Asserts not defined for `#{ec.setup_key}`. Defined: #{@assertions.keys}" unless @assertions[key]
+        ec.asserts_keys.each do |key|
+          raise "Asserts not defined for `#{ec.setup_key}`. Defined: #{@assertions.keys}" unless @assertions[key]
+        end
       end
     end
-  end
 
-  def lock!
-    @equivalence_classes.each do |ec|
-      ec.setup        = @setup_set_hash[ec.setup_key] || SetupSet.new('ec.setup_key', nil)
-      ec.action_block = @action_block
-      ec.assertions   = ec.asserts_keys.map { |desc| @assertions[desc] }
+    def lock!
+      @equivalence_classes.each do |ec|
+        ec.setup        = @setup_set_hash[ec.setup_key] || SetupSet.new('ec.setup_key', nil)
+        ec.action_block = @action_block
+        ec.assertions   = ec.asserts_keys.map { |desc| @assertions[desc] }
+      end
     end
-  end
 
-  def each_equivalence_class(&block)
-    @equivalence_classes.each { |kase| block.call(kase) }
+    def each_equivalence_class(&block)
+      @equivalence_classes.each { |kase| block.call(kase) }
+    end
   end
 end
 
 def thoreau(&block)
-  suite = SuiteDSL.new
+  suite = Thoreau::SuiteDSL.new
   suite.instance_eval(&block)
   suite.verify_config!
   suite.lock!
@@ -196,7 +197,7 @@ thoreau do
 end
 
 
-describe SetupSet do
+describe Thoreau::SetupSet do
 
   thoreau do
 
@@ -223,7 +224,7 @@ describe SetupSet do
     setup 'nil', nil
 
     action do |input|
-      subject = SetupSet.new('desc', input)
+      subject = Thoreau::SetupSet.new('desc', input)
       [].tap do |result|
         subject.each_setup_block { |b| result << b.call }
       end
@@ -236,7 +237,7 @@ describe SetupSet do
   end
 end
 
-describe 'Assertion' do
+describe Thoreau::Assertion do
   thoreau do
 
     cases '`exec_in_context`' => [
@@ -246,7 +247,7 @@ describe 'Assertion' do
     ]
 
     setup '`exec_in_context`' do
-      Assertion.new('desc', -> (result, setup_value) do
+      Thoreau::Assertion.new('desc', -> (result, setup_value) do
         @foo         = 'bar'
         @setup_value = setup_value
         @result      = result
