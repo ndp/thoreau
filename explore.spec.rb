d73dc7897fb1397762a161d46989f869ff733025
Andrew Peterson <andy@ndpsoftware.com>
2020-01-25 00:58:03 -0800

    Tests for Assertion

--- explore.spec.rb
+++ explore.spec.rb
@@ -235,6 +235,40 @@ describe SetupSet do
   end
 end
 
+describe 'Assertion' do
+  thoreau do
+
+    cases '`exec_in_context`' => [
+      'calls block given in context',
+      'passes in `setup_value` to block given',
+      'passes in `result` to block given'
+    ]
+
+    setups '`exec_in_context`' do
+      Assertion.new('desc', -> (result, setup_value) do
+        @foo    = 'bar'
+        @setup_value  = setup_value
+        @result = result
+      end)
+    end
+
+    action do |subject|
+      @context = Object.new
+      subject.exec_in_context(@context, 'result', 'setup value')
+    end
+
+    asserts 'calls block given in context' do
+      @context.instance_variable_get(:@foo).must_be :==, 'bar'
+    end
+    asserts 'passes in `setup_value` to block given' do
+      @context.instance_variable_get(:@setup_value).must_be :==, 'setup value'
+    end
+    asserts 'passes in `result` to block given' do
+      @context.instance_variable_get(:@result).must_be :==, 'result'
+    end
+  end
+end
+
 describe 'dsl' do
 
   thoreau do
