6b95180e9df4993a7eebec4750d84bad989a48a3
Andrew Peterson <andy@ndpsoftware.com>
2020-02-22 13:11:18 -0800

    More examples

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,18 +1,18 @@
 require_relative './spec_helper'
 
-def example(x)
+def example_fn(x)
   return nil if x.is_a?(String)
   x * 2 rescue nil
 end
 
 
-RSpec.describe 'Thoreau: Example using implicit data' do
+RSpec.describe 'Example using implicit data' do
 
   cases 'any integer': 'doubles the input',
         'nil input':   'returns nil',
         'any string':  'returns nil'
 
-  action { |input| example(input) }
+  action { |input| example_fn(input) }
 
   setup('any integer', [0, -1, 1, 1 << 32, -(1 << 32)])
   setup 'nil input', nil
@@ -30,13 +30,13 @@ RSpec.describe 'Thoreau: Example using implicit data' do
 
 end
 
-RSpec.describe 'Thoreau: Example using named variables' do
+RSpec.describe 'Example using named variables' do
 
   cases 'any integer `i`':  'doubles the input',
         'nil input':        'returns nil',
         'any string input': 'returns nil'
 
-  action { example(i) }
+  action { example_fn(i) }
 
   setup('any integer `i`', i: [0, -1, 1, 1 << 32, -(1 << 32)])
   setup 'nil input', i: nil
@@ -48,6 +48,34 @@ RSpec.describe 'Thoreau: Example using named variables' do
 
   asserts 'returns nil' do |actual|
     expect(actual).to eq nil
+  end
+
+  generate!
+
+end
+
+RSpec.describe 'Example using setup blocks' do
+
+  cases 'an integer':     'doubles the input',
+        'nil input':      'returns nil',
+        'a string input': 'returns nil'
+
+  action { @result = example_fn(@i) }
+
+  setup('an integer') do
+    @i = 13
+  end
+  setup 'nil input'
+  setup('a string input') do
+    @i = 'foo'
+  end
+
+  asserts 'doubles the input' do
+    expect(@result).to eq 26
+  end
+
+  asserts 'returns nil' do
+    expect(@result).to eq nil
   end
 
   generate!
